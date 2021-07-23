5a00029f80e2771e82b192c9d5327a70852ab20d
Tien Huynh <anhtienzz123@gmail.com>
2021-07-23 15:00:47 +0700

    fix enable cors

--- src/main/java/webtienganh/controller/VideoCategoryController.java
+++ src/main/java/webtienganh/controller/VideoCategoryController.java
@@ -3,6 +3,7 @@ package webtienganh.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import webtienganh.service.VideoCategoryService;
 
 @RestController
 @RequestMapping("/video-categories")
+@CrossOrigin
 public class VideoCategoryController {
 
 	@Autowired
