d6af4e7d7c113331bea71284c01b30a00517284a
Tien Huynh <anhtienzz123@gmail.com>
2021-07-30 11:53:28 +0700

    fix cors for meController

--- src/main/java/webtienganh/controller/MeController.java
+++ src/main/java/webtienganh/controller/MeController.java
@@ -1,6 +1,7 @@
 package webtienganh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import webtienganh.service.MeService;
 
 @RestController
 @RequestMapping("/user/me")
+@CrossOrigin
 public class MeController {
 
 	@Autowired
--- src/main/java/webtienganh/controller/admin/BlogAdminController.java
+++ src/main/java/webtienganh/controller/admin/BlogAdminController.java
@@ -1,11 +1,13 @@
 package webtienganh.controller.admin;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/blogs")
+@CrossOrigin
 public class BlogAdminController {
 
 	@GetMapping()
