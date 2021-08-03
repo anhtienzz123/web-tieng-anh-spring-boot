3edb8d7f7721d5a026f19a1f318bea286d563aa5
Tien Huynh <anhtienzz123@gmail.com>
2021-08-03 18:32:57 +0700

    api CRUD topic

--- src/main/java/webtienganh/service/impl/TopicServiceImpl.java
+++ src/main/java/webtienganh/service/impl/TopicServiceImpl.java
@@ -1,6 +1,8 @@
 package webtienganh.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Service;
 
 import webtienganh.converter.TopicConverter;
 import webtienganh.dto.TopicDTO;
+import webtienganh.entity.Topic;
+import webtienganh.exception.MyExceptionHelper;
 import webtienganh.repository.TopicRepository;
 import webtienganh.service.TopicService;
+import webtienganh.utils.MyConstant;
 
 @Service
 @Transactional
@@ -27,5 +32,43 @@ public class TopicServiceImpl implements TopicService {
 	public List<TopicDTO> getAll() {
 
 		return topicRepository.findAll().stream().map(tp -> topicConverter.toTopicDTO(tp)).collect(Collectors.toList());
+	}
+
+	@Override
+	public TopicDTO save(TopicDTO topicDTO) {
+
+		validate(topicDTO);
+
+		Topic topicWasSave = topicRepository.save(topicConverter.toTopic(topicDTO));
+
+		return topicConverter.toTopicDTO(topicWasSave);
+	}
+
+	private void validate(TopicDTO topicDTO) {
+		if (topicDTO == null || topicDTO.getId() < 0)
+			throw MyExceptionHelper.throwIllegalArgumentException();
+
+		Integer id = topicDTO.getId();
+
+		if (id != 0 && !topicRepository.existsById(id))
+			throw MyExceptionHelper.throwResourceNotFoundException(MyConstant.TOPIC);
+
+		if (topicRepository.existsByIdNotAndName(id, topicDTO.getName())) {
+			Map<String, String> error = new HashMap<>();
+			error.put("name", "Tên topic bị trùng");
+			throw MyExceptionHelper.throwRuntimeCustomException(error);
+		}
+	}
+
+	@Override
+	public void delete(Integer id) {
+
+		if (id == null || id <= 0)
+			throw MyExceptionHelper.throwIllegalArgumentException();
+
+		if (!topicRepository.existsById(id))
+			throw MyExceptionHelper.throwResourceNotFoundException(MyConstant.TOPIC);
+
+		topicRepository.deleteById(id);
 	}
 }
